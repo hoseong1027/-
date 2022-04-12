da065164d5c6d66d802d9ccbebbd581974bc9e9e
이호성 <dlghtjd1027@naver.com>
2022-04-12 00:05:18 +0000

    1번 파일 수정

---
+++ 30407 test/HelloEx002.java
@@ -0,0 +1,8 @@
+public class HelloEx002{
+    public static void main(String[] args){
+        System.out.print(100);
+        System.out.println(100);
+        System.out.print("합격점수:"+100);
+        
+    }
+}
